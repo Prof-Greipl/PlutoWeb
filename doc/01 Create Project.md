265ef662e50ebb297beedae3b9d0251a4c713b8c
Prof-Greipl <dieter.greipl@haw-landshut.de>
2022-10-23 19:02:14 +0200

    Inital

---
+++ doc/01 Create Project.md
@@ -0,0 +1,174 @@
+## Create Project
+
+### Node.js
+
+Install / update Node.js to the latest version (16.18.0 in 10/2022).
+
+![image-20221022143848728](assets/01 Create Project.assets/image-20221022143848728.png)
+
+After installarion check your node version using
+
+```
+node -v
+```
+
+
+
+### Init Project
+
+```bash
+npm init
+```
+
+This creates the file `package.json`.
+
+
+
+### Firebase
+
+#### Firebase Tools
+
+Run 
+
+```
+firebase --version
+```
+
+If the command is not found or you get an upgrade hint, run
+
+```
+npm install -h firebase-tools
+```
+
+Note: You might get a warning due to outdated Node.js version. In this case update Node.js first.
+
+Check (https://firebase.google.com/docs/cli#update-cli).
+
+
+
+#### Install Firebase
+
+```bash
+npm install firebase@9.12.1
+```
+
+This creates the folder `node_modules` and updates the `package.json` file.
+
+![image-20221022141300387](assets/01 Create Project.assets/image-20221022141300387.png)
+
+package.json
+
+```json
+{
+  "dependencies": {
+    "firebase": "^9.12.1"
+  }
+}
+```
+
+node-modules contains all files for firebase.
+
+### Module Bundling (`webpack`)
+
+https://firebase.google.com/docs/web/module-bundling
+
+#### Install `webpack`
+
+```
+npm i webpack webpack-cli -D
+```
+
+This installs `webpack` in the node_modules folder.
+
+![image-20221022162932552](assets/01 Create Project.assets/image-20221022162932552.png)
+
+The update package.json with the line in the `scripts` section
+
+```
+"build": "webpack --mode=development"
+```
+
+#### Add config.file
+
+```javascript
+const path = require('path');
+
+module.exports = {
+  // The entry point file described above
+  entry: './index.js',
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+     
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: 'eval-source-map',
+};
+```
+
+##### Enable CSS Loading
+
+Add this to the config-file
+
+```
+module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: ['style-loader', 'css-loader'],
+       },
+     ],
+   },
+```
+
+
+
+## Demo App
+
+### V9
+
+https://firebase.google.com/docs/web/modular-upgrade
+
+### Add Firebase UI
+
+```
+$ npm install firebaseui
+```
+
+
+
+## Project Hosting
+
+Link for this chapter: https://firebase.google.com/docs/hosting/quickstart 
+
+### Connect Project to Firebase Project
+
+Run 
+
+```
+firebas init hosting
+```
+
+and answer the questions. Your lecturer will support you. This will create a file with name `firebase.json`
+
+```json
+{
+  "hosting": {
+    "public": "public",
+    "ignore": [
+      "firebase.json",
+      "**/.*",
+      "**/node_modules/**"
+    ]
+  }
+}
+```
+
+### Deploy the project
+
+```bash
+firebase deploy --only hosting
+```
+
---
+++ src/index.js
@@ -0,0 +1,171 @@
+// Import stylesheets
+//import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp, firebase } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {
+  getAuth,
+  EmailAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+} from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const title = document.getElementById('title');
+const body = document.getElementById('body');
+const postSection = document.getElementById('postSection');
+
+const buSignIn = document.getElementById('buSignIn');
+const buSignOut = document.getElementById('buSignOut');
+const buPost = document.getElementById('buPost');
+const buManageAccount = document.getElementById('buManageAccount');
+const buSubmit = document.getElementById('buSubmit');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  // Add Firebase project configuration object here
+  const firebaseConfig = {
+    apiKey: 'AIzaSyD1M3cwjvRdl-JpC4RoR8JStjKOOi7kdkE',
+    authDomain: 'pluto22-gkw.firebaseapp.com',
+    databaseURL: 'https://pluto22-gkw-default-rtdb.firebaseio.com',
+    projectId: 'pluto22-gkw',
+    storageBucket: 'pluto22-gkw.appspot.com',
+    messagingSenderId: '84704125590',
+    appId: '1:84704125590:web:0eafedfc271c4c86db98b7',
+  };
+
+  initializeApp(firebaseConfig);
+
+  auth = getAuth();
+  db = getFirestore();
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      },
+    },
+  };
+
+  const ui = new firebaseui.auth.AuthUI(auth);
+
+  // Event Listener for Buttons
+
+  buSignIn.addEventListener('click', () => {
+    if (auth.currentUser) {
+      // User is signed in; allows user to sign out
+      signOut(auth);
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  });
+
+  buSignOut.addEventListener('click', () => {
+    if (auth.currentUser) {
+      // User is signed in; allows user to sign out
+      signOut(auth);
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  });
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('onAuthStateChanged: User present');
+      buSignOut.style.display = 'block';
+      buSignIn.style.display = 'none';
+      buManageAccount.style.display = 'block';
+    } else {
+      console.log('onAuthStateChanged: No user present');
+      buSignOut.style.display = 'none';
+      buSignIn.style.display = 'block';
+      buManageAccount.style.display = 'none';
+    }
+  });
+
+  // Listen to the form submission
+  form.addEventListener('submit', async (e) => {
+    // Prevent the default form redirect
+    e.preventDefault();
+    // Write a new message to the database collection "guestbook"
+    console.log('Writing to db');
+    addDoc(collection(db, 'posts'), {
+      author: auth.currentUser.email,
+      title: title.value,
+      body: body.value,
+      createdAt: new Date(), //firebase.firestore.FieldValue.serverTimestamp(),
+      source: 'Web',
+      uid: auth.currentUser.uid,
+    })
+      .then(() => {
+        console.log('Successfzlly written.');
+      })
+      .catch((error) => {
+        console.log('Adding doc failed: ', error);
+      });
+
+    return false;
+  });
+
+  // Create query for messages
+  const q = query(collection(db, 'posts'), orderBy('createdAt', 'desc'));
+  onSnapshot(q, (snaps) => {
+    // Reset page
+    postSection.innerHTML = '';
+    // Loop through documents in database
+
+    snaps.forEach((doc) => {
+      const entry = document.createElement('div');
+      entry.className = 'entry';
+
+      const line1 = document.createElement('div');
+      line1.textContent = doc.data().title;
+      line1.className = 'title';
+      entry.appendChild(line1);
+
+      const line2 = document.createElement('div');
+      line2.textContent = doc.data().body;
+      line2.className = 'body';
+      entry.appendChild(line2);
+
+      const ruler = document.createElement('hr');
+      entry.appendChild(ruler);
+
+      postSection.appendChild(entry);
+    });
+  });
+}
+
+main();
+title.value = 'Hans';
+body.value = 'Lore Ipsum se...';
